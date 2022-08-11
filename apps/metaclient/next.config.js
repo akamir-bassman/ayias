eeedd6a061a6f0557c3621a4ac9b8d8bf03348a8
unknown <bassman@akamir.com>
2022-08-11 19:07:40 +0900

    a

--- apps/metaclient/next.config.js
+++ apps/metaclient/next.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNx = require('@nrwl/next/plugins/with-nx');
+const withNx = require("@nrwl/next/plugins/with-nx");
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -9,6 +9,38 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+    images: {
+      domains: [
+        "akamir.com",
+        "asset.akamir.com",
+        "s3.ap-northeast-2.amazonaws.com",
+        "asset.ayias.io",
+        "dev-asset.ayias.io",
+        "api.ayias.io",
+        "dev.akamir.s3.ap-northeast-2.amazonaws.com",
+      ],
+    },
+  },
+  async headers() {
+    return [
+      {
+        source: "/path",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/ayias/:path*",
+        destination: "https://asset.ayias.io/:path*",
+      },
+    ];
   },
 };
 
