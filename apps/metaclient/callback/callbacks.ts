eaa92c7d6eb29a6ada8e2147e9ae8cebf3dd6193
unknown <bassman@akamir.com>
2022-08-11 17:08:38 +0900

    a

--- apps/metaclient/callback/callbacks.ts
+++ apps/metaclient/callback/callbacks.ts
@@ -1,11 +1,17 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
-import Reactverse, { types, ReactverseProps } from "reactverse";
+import Reactverse, { types, ReactverseProps } from "@decentverse/client";
 import { Socket as Soc } from "socket.io-client";
 const Caver = require("caver-js");
-import { Utils } from "@akamir/common";
 import { ethers } from "ethers";
 import * as abi from "./abi";
-export const donation: types.ItemCallback = async (store: types.AllStore, socket: Soc, id: string, option: any) => {
+export const shuffle = <T>(arr: T[]): T[] =>
+  arr.sort((a, b) => 0.5 - Math.random());
+export const donation: types.ItemCallback = async (
+  store: types.AllStore,
+  socket: Soc,
+  id: string,
+  option: any
+) => {
   store.world.getState().pendingStart();
   const inventory = store.inventory.getState().inventory;
   const userId = store.world.getState().me.id;
@@ -13,17 +19,33 @@ export const donation: types.ItemCallback = async (store: types.AllStore, socket
   const num = option?.num ?? 1;
   const { chain, address, type } = item.item.contract;
   if (chain === "ethereum") {
-    const [selectedAddress]: string[] = await window.ethereum.request({ method: "eth_requestAccounts" });
+    const [selectedAddress]: string[] = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
     const provider = new ethers.providers.Web3Provider(window.ethereum as any);
-    const contract = new ethers.Contract(item.item.contract.address, item.item.contract.abi, provider.getSigner(0));
+    const contract = new ethers.Contract(
+      item.item.contract.address,
+      item.item.contract.abi,
+      provider.getSigner(0)
+    );
     const to = process.env.NEXT_PUBLIC_GET_DONATE_ADDRESS;
     if (typeof window.ethereum === "undefined") return;
     if (type === "ERC-721") {
-      const tx = await contract.safeTransferFrom(selectedAddress, to, item.item.tokenId);
+      const tx = await contract.safeTransferFrom(
+        selectedAddress,
+        to,
+        item.item.tokenId
+      );
       await tx.wait();
     } else if (type === "ERC-1155") {
       const data = "0x0";
-      const tx = await contract.safeTransferFrom(selectedAddress, to, item.item.tokenId, num, data);
+      const tx = await contract.safeTransferFrom(
+        selectedAddress,
+        to,
+        item.item.tokenId,
+        num,
+        data
+      );
       await tx.wait();
     }
   } else if (chain === "klaytn") {
@@ -36,27 +58,34 @@ export const donation: types.ItemCallback = async (store: types.AllStore, socket
       const to = process.env.NEXT_PUBLIC_GET_DONATE_ADDRESS;
       if (type === "ERC-721") {
         const safetrnasferAbi = abi.safetransferFrom721;
-        const contract = new caver.klay.Contract(safetrnasferAbi, item.item.contract.address);
+        const contract = new caver.klay.Contract(
+          safetrnasferAbi,
+          item.item.contract.address
+        );
 
-        const rst = await contract.methods["safeTransferFrom(address,address,uint256)"](account, to, item.item.tokenId)
+        const rst = await contract.methods[
+          "safeTransferFrom(address,address,uint256)"
+        ](account, to, item.item.tokenId)
           .send({ from: account, gas: 300000 })
           .then(function (result) {
             console.log(result);
           });
       } else if (type === "ERC-1155") {
         const safetrnasferAbi = abi.safetransferFrom1155;
-        const contract = new caver.klay.Contract(safetrnasferAbi, item.item.contract.address);
+        const contract = new caver.klay.Contract(
+          safetrnasferAbi,
+          item.item.contract.address
+        );
         const data = "0x0";
-        const rst = await contract.methods["safeTransferFrom(address,address,uint256,uint256,bytes)"](
-          account,
-          to,
-          item.item.tokenId,
-          num,
-          data
-        )
-          .send({ from: account, gas: 300000 }, function (error, transactionHash) {
-            console.log(error, transactionHash);
-          })
+        const rst = await contract.methods[
+          "safeTransferFrom(address,address,uint256,uint256,bytes)"
+        ](account, to, item.item.tokenId, num, data)
+          .send(
+            { from: account, gas: 300000 },
+            function (error, transactionHash) {
+              console.log(error, transactionHash);
+            }
+          )
           .on("error", function (error) {
             console.log(error);
           })
@@ -79,7 +108,11 @@ export const donation: types.ItemCallback = async (store: types.AllStore, socket
     } catch (err) {
       console.log(err);
       store.world.getState().pendingEnd();
-      store.inventory.setState({ isOpenInventory: false, isOpenItemMenu: false, isShowItemInfo: false });
+      store.inventory.setState({
+        isOpenInventory: false,
+        isOpenItemMenu: false,
+        isShowItemInfo: false,
+      });
 
       return;
     }
@@ -87,13 +120,25 @@ export const donation: types.ItemCallback = async (store: types.AllStore, socket
     console.log("luniverse");
   }
   store.world.getState().pendingEnd();
-  setTimeout(async () => await store.inventory.getState().syncInventory(userId), 1000);
+  setTimeout(
+    async () => await store.inventory.getState().syncInventory(userId),
+    1000
+  );
   store.visualEffect.setState({ effectType: "donationOut" });
-  store.inventory.setState({ isOpenInventory: false, isOpenItemMenu: false, isShowItemInfo: false });
+  store.inventory.setState({
+    isOpenInventory: false,
+    isOpenItemMenu: false,
+    isShowItemInfo: false,
+  });
   setTimeout(() => store.visualEffect.setState({ effectType: "none" }), 3000);
   return;
 };
-export const goldenbell: types.ItemCallback = async (store: types.AllStore, socket: Soc, id: string, option: any) => {
+export const goldenbell: types.ItemCallback = async (
+  store: types.AllStore,
+  socket: Soc,
+  id: string,
+  option: any
+) => {
   store.world.getState().pendingStart();
   const inventory = store.inventory.getState().inventory;
   const item = inventory.find((cur) => cur?.item?.id === id);
@@ -110,54 +155,59 @@ export const goldenbell: types.ItemCallback = async (store: types.AllStore, sock
   const tempTargets = otherPlayerIds
     .map((id) => {
       const otherPlayer = otherPlayers.get(id)?.user;
-      const wallet = otherPlayer.wallets.find((wallet) => wallet.chain === chain);
+      const wallet = otherPlayer.wallets.find(
+        (wallet) => wallet.chain === chain
+      );
       if (!wallet) return null;
       return { id: otherPlayer.id, address: wallet.address };
     })
     .filter((cur) => cur && cur?.id !== user.id);
-  const targets = Utils.shuffle(tempTargets).slice(0, num);
-  const addresslist = targets.map((target) => target.address);
+  const targets = shuffle(tempTargets).slice(0, num);
+  const addresslist = targets.map((target: any) => target.address);
   if (chain === "klaytn") {
     const [account] = await window.klaytn.enable();
     const caver = new Caver(window.klaytn);
     const approvalForAllAbi = abi.approvalForAllAbi;
     const transferAbi = abi.transferContractAbi;
-    const transferContractAddr = process.env.NEXT_PUBLIC_TRANSFER_CONTRACT_ADDRESS;
+    const transferContractAddr =
+      process.env.NEXT_PUBLIC_TRANSFER_CONTRACT_ADDRESS;
     const contract = new caver.klay.Contract(approvalForAllAbi, address);
-    const transferContract = new caver.klay.Contract(transferAbi, transferContractAddr);
-    const gasAmount2 = await contract.methods.setApprovalForAll(transferContractAddr, true).estimateGas({
-      from: account,
-    });
-    try {
-      const isApproved = await contract.methods.isApprovedForAll(account, transferContractAddr).call({
+    const transferContract = new caver.klay.Contract(
+      transferAbi,
+      transferContractAddr
+    );
+    const gasAmount2 = await contract.methods
+      .setApprovalForAll(transferContractAddr, true)
+      .estimateGas({
         from: account,
       });
+    try {
+      const isApproved = await contract.methods
+        .isApprovedForAll(account, transferContractAddr)
+        .call({
+          from: account,
+        });
 
       !isApproved &&
-        (await contract.methods.setApprovalForAll(transferContractAddr, true).send({
-          from: account,
-          gas: gasAmount2,
-        }));
+        (await contract.methods
+          .setApprovalForAll(transferContractAddr, true)
+          .send({
+            from: account,
+            gas: gasAmount2,
+          }));
 
       if (item.item.contract.type === "ERC-721") {
-        const ret = await transferContract.methods["goldenbellKIP17(address,address,address[],uint256[])"](
-          address,
-          account,
-          addresslist,
-          [tokenId]
-        ).send({
+        const ret = await transferContract.methods[
+          "goldenbellKIP17(address,address,address[],uint256[])"
+        ](address, account, addresslist, [tokenId]).send({
           from: account,
           gas: 300000,
         });
       } else if (item.item.contract.type === "ERC-1155") {
         const amounts = addresslist.map(() => 1);
-        const ret = await transferContract.methods["goldenbellERC1155(address,address,uint256,address[],uint256[])"](
-          address,
-          account,
-          tokenId,
-          addresslist,
-          amounts
-        )
+        const ret = await transferContract.methods[
+          "goldenbellERC1155(address,address,uint256,address[],uint256[])"
+        ](address, account, tokenId, addresslist, amounts)
           .send({
             from: account,
             gas: 300000,
@@ -178,9 +228,19 @@ export const goldenbell: types.ItemCallback = async (store: types.AllStore, sock
 
   store.world.getState().pendingEnd();
 
-  setTimeout(async () => await store.inventory.getState().syncInventory(user.id), 1000);
-  socket.emit("events", { userIds: targets.map((target) => target.id), type: "effect" });
-  store.inventory.setState({ isOpenInventory: false, isOpenItemMenu: false, isShowItemInfo: false });
+  setTimeout(
+    async () => await store.inventory.getState().syncInventory(user.id),
+    1000
+  );
+  socket.emit("events", {
+    userIds: targets.map((target: any) => target.id),
+    type: "effect",
+  });
+  store.inventory.setState({
+    isOpenInventory: false,
+    isOpenItemMenu: false,
+    isShowItemInfo: false,
+  });
   store.visualEffect.setState({ effectType: "goldenBell" });
   setTimeout(() => store.visualEffect.setState({ effectType: "none" }), 3000);
 };
@@ -191,7 +251,10 @@ export const eventCallback: types.scalar.EventCallback = async (
   const userId = store.world.getState().me.id;
   if (data.type === "effect" && data.userIds.includes(userId)) {
     //setEffect(1);
-    setTimeout(async () => await store.inventory.getState().syncInventory(userId), 1000);
+    setTimeout(
+      async () => await store.inventory.getState().syncInventory(userId),
+      1000
+    );
     store.visualEffect.setState({ effectType: "donationIn" });
     setTimeout(() => store.visualEffect.setState({ effectType: "none" }), 3000);
   }
